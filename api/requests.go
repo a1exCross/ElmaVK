b316124cbef6f8b435c04b842838e04719a47678
a1exCross <sashek2000@bk.ru>
2022-06-09 01:07:03 +0300

    v4

--- api/requests.go
+++ api/requests.go
@@ -10,7 +10,6 @@ const api_host = "https://api.vk.com/method/"
 
 func (v VK) Reqeust_api_get(method, param string) (*http.Response, error) {
 	req, err := http.NewRequest(http.MethodGet, api_host+method+param, nil)
-
 	if err != nil {
 		return nil, err
 	}
@@ -20,7 +19,6 @@ func (v VK) Reqeust_api_get(method, param string) (*http.Response, error) {
 
 func (v VK) Reqeust_api_post(method, param string, data url.Values) (*http.Response, error) {
 	req, err := http.NewRequest(http.MethodPost, api_host+method+param, strings.NewReader(data.Encode()))
-
 	if err != nil {
 		return nil, err
 	}
